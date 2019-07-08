fa5455eb914fde3361a3bb16e85e057d0bfb4e48
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-07-08 12:13:58 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,4 +3,11 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
   has_many :notes, through: :songs
 
+  def artist_name=(name)
+     self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
 end
